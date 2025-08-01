95f4db4c81e1abe1a6e516432ffda0b59335de8c
mhumza37dev <mhumza37@gmail.com>
2025-08-01 05:04:49 +0500

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,11 +7,11 @@ interface DatabaseConfig {
 
 const config: DatabaseConfig = {
   development: {
-    username: process.env.username || 'postgres',
-    password: process.env.password || '1234',
-    database: process.env.dbname || 'atlas-ai-local',
-    host: process.env.host || 'localhost',
-    port: parseInt(process.env.port || '5432'),
+    username: process.env.DB_USER || process.env.username || 'postgres',
+    password: process.env.DB_PASSWORD || process.env.password || '1234',
+    database: process.env.DB_NAME || process.env.dbname || 'atlas-ai-local',
+    host: process.env.DB_HOST || process.env.host || 'localhost',
+    port: parseInt(process.env.DB_PORT || process.env.port || '5432'),
     dialect: 'postgres',
     logging: console.log,
     models: [path.join(__dirname, '../models/**/*.model.ts')],
@@ -20,11 +20,11 @@ const config: DatabaseConfig = {
     },
   },
   production: {
-    username: process.env.username,
-    password: process.env.password,
-    database: process.env.dbname,
-    host: process.env.host,
-    port: parseInt(process.env.port || '5432'),
+    username: process.env.DB_USER || process.env.username,
+    password: process.env.DB_PASSWORD || process.env.password,
+    database: process.env.DB_NAME || process.env.dbname,
+    host: process.env.DB_HOST || process.env.host,
+    port: parseInt(process.env.DB_PORT || process.env.port || '5432'),
     dialect: 'postgres',
     logging: false,
     ssl: true,
