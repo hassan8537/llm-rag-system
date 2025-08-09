f5ed1c9ce068ebf43ea4aab6991fc5851a17527f
mhumza37dev <mhumza37@gmail.com>
2025-08-09 15:24:08 +0500

    updated code

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 interface DatabaseConfig {
   development: any;
   production: any;
@@ -5,30 +7,35 @@ interface DatabaseConfig {
 
 const config: DatabaseConfig = {
   development: {
-    username: "postgres",
-    password: "12345",
-    database: "atlas-ai-local",
-    host: "localhost",
-    port: 5432,
-    dialect: "postgres",
+    username: process.env.DB_USER || process.env.username || 'postgres',
+    password: process.env.DB_PASSWORD || process.env.password || '1234',
+    database: process.env.DB_NAME || process.env.dbname || 'atlas-ai-local',
+    host: process.env.DB_HOST || process.env.host || 'localhost',
+    port: parseInt(process.env.DB_PORT || process.env.port || '5432'),
+    dialect: 'postgres',
     logging: console.log,
+    models: [path.join(__dirname, '../models/**/*.model.ts')],
+    modelMatch: (filename: string, member: string) => {
+      return filename.substring(0, filename.indexOf('.model')) === member.toLowerCase();
+    },
   },
   production: {
     username: process.env.DB_USER || process.env.username,
     password: process.env.DB_PASSWORD || process.env.password,
     database: process.env.DB_NAME || process.env.dbname,
     host: process.env.DB_HOST || process.env.host,
-    port: 5432,
-    dialect: "postgres",
+    port: parseInt(process.env.DB_PORT || process.env.port || '5432'),
+    dialect: 'postgres',
     logging: false,
     ssl: true,
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
-      },
+        rejectUnauthorized: false
+      }
     },
-  },
+    models: [path.join(__dirname, '../models/**/*.model.js')],
+  }
 };
 
 export default config;
